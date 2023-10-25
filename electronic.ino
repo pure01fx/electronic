a374522716c8613478fc751eaa595e0bee27ad1e
fx Lingyi <pure@01fx.icu>
2023-10-25 23:57:15 +0800

    feat: add servo

--- electronic.ino
+++ electronic.ino
@@ -1,14 +1,18 @@
 #include "QGPMaker_IICSensorbar.h"
 #include "QGPMaker_MotorShield.h"
 
-QGPMaker_MotorShield motorShield = new QGPMaker_MotorShield(0x60);
+QGPMaker_MotorShield motorShield = new QGPMaker_MotorShield();
+QGPMaker_Servo *servo1 = motorShield.getServo(1);
 
 void setup() {
   // put your setup code here, to run once:
-
+  motorShield.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  servo1->writeServo(10);
+  delay(1000);
+  servo1->writeServo(120);
+  delay(1000);
 }
